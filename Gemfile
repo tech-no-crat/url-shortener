50efff5fba5c0e93975a43145c4add972e2c4a38
Christos Porios <porios@yahoo.gr>
2013-02-12 22:16:04 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source "http://rubygems.org"
+
+gem 'sinatra'
+gem 'rack'
+gem 'haml'
+gem 'sass'
+gem 'redis'
+gem 'coffee-script'
+gem 'therubyracer'
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+  gem 'mock_redis'
+end
+
---
+++ url_shortener.rb
@@ -0,0 +1,53 @@
+require 'sinatra'
+require 'redis'
+require 'haml'
+require 'coffee_script'
+require 'sass'
+require 'yaml'
+
+configure do
+  CONFIG = YAML.load_file('config.yaml')
+  REDIS = Redis.new(CONFIG['redis'])
+end
+
+def save(id, link)
+  if (id =~ /^[a-zA-Z0-9\-_]*$/) != 0 or id.length<3 or not find(id).nil?
+    status 400 # Bad Request
+    "Error: id invalid or already in use"
+  elsif link.length < 3 or not link =~ %r{\Ahttps?://}
+    status 400 # Bad Request
+    "Error: invalid link"
+  else 
+    if REDIS.set(id, link) == "OK"
+      status 201 # Created
+      "OK: #{CONFIG['domain']}/#{id}"
+    else
+      status 500 # Internal Server Error
+      "Error: could not save to Redis"
+    end
+  end
+end
+
+def find(id)
+  REDIS.get id
+end
+
+get "/" do
+  erb :new
+end
+
+get "/:id" do |id|
+  link = find(id)
+  if link
+      redirect link
+  else
+    status 404
+    "Error: Link not found."
+  end
+end
+
+post "/" do
+  id = params[:id]
+  link = params[:link]
+  save(id, link)
+end
